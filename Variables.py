6dc1fc47b39cc238dadf9cb22cc042a8cc54626e
MediPavani <medipavani021@gmail.com>
2025-06-30 15:20:31 +0530

    added

---
+++ Variables.py
@@ -0,0 +1,29 @@
+#a=10
+#b,c=20,30
+#d=e=f=40
+#print(a)
+#print(b)
+#print(c)
+#print(d)
+#a=int(input("Enter a value:"))
+#b=int(input("Enter b value:"))
+#print(a)
+#print(a+b)
+#d,e=map(int,input("Enter d and e value:").split())
+#print(d)
+#age=18
+#if age>18:
+ #   print("Eligible")
+#elif age==18:
+  #  print("Welcome new Voter")
+#else:
+ #   print("Not Eligible")
+#print("If condition Execution")
+#no=int(input("Enter a number:"))
+#if(no//10==7 or no%10==7 ):
+#    print("Lucky No")
+#else:
+ #   print("Not Lucky")
+for i in range(10,100):
+    if(i%10==7 or i//10==7):
+        print(i,end=",")
